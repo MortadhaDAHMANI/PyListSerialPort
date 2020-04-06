463fa026c66a7ca88afd56cae04d43d61303d8be
Mortadha DAHMANI <40170728+MortadhaDAHMANI@users.noreply.github.com>
2020-04-06 18:51:04 +0100

    Create ListSerialPort.py

---
+++ ListSerialPort.py
@@ -0,0 +1,23 @@
+import os
+
+import serial
+import serial.tools.list_ports
+
+ports = serial.tools.list_ports.comports(include_links=False)
+
+for port in ports :
+    print(port.device)
+print("--------------------")
+#
+
+for port, desc, hwid in sorted(ports):
+        print("{}: {} [{}]".format(port, desc, hwid))
+print("--------------------")
+#
+
+comlist = serial.tools.list_ports.comports()
+connected = []
+for element in comlist:
+    connected.append(element.device)
+print("Connected COM ports: " + str(connected))
+print("--------------------")
